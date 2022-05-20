11cce7b21c374f001f73f49253066752843a1caa
maxhandx7 <alancarabali@gmail.com>
2022-05-20 13:00:10 -0500

    actualizacion estado e imagenes

--- Vistas/Admin/config.php
+++ Vistas/Admin/config.php
@@ -2,7 +2,8 @@
 $ObjectC = new Conexion();
 $conexion = $ObjectC->Connect();
 $lol = $user->getID();
-$sql = "SELECT image FROM imagen WHERE users_id = $lol  ORDER BY image ASC";
-$sql2 = "SELECT estado FROM esatdo WHERE users_id = $lol  ORDER BY estado ASC";
+$sql = "SELECT image FROM imagen WHERE users_id = $lol  ORDER BY imagen.id ASC";
+$sql2 = "SELECT estado FROM esatdo WHERE users_id = $lol ORDER BY esatdo.id ASC";
+
 $result = mysqli_query($conexion, $sql);
 $result2 = mysqli_query($conexion, $sql2);
--- home.php
+++ home.php
@@ -19,8 +19,10 @@
             <h3 class="display-5 text-center"><b><span><?php echo $user->getNombre(); ?></span></b></h3>
             <p class="lead text-center"><?php echo $user->getEmail(); ?></p>
             <hr class="my-4">
-            <div class="row">
-                <div class="col-sm-6">
+        </div>
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="card">
                     <?php
 
                     foreach ($result as $r) {
@@ -28,41 +30,45 @@
                     }
                     if (empty($r)) {
                         echo "<img  src='Public/images/user.png' height='300px' width='300px' style='border-radius: 50%;  display: block;
-            margin: 0px auto;' alt='profile'>";
+                        margin: 0px auto;' alt='profile'>";
                     } else {
-                        echo "<img  src='Public/images/" . $r['image'] . "' height='300px' width='300px' style='border-radius: 50%;  display: block;
-                margin: 0px auto;' alt='profile'>";
+                        echo "<img  src='Public/images/" . $r['image'] . "' height='300px' width='300px' style='border-radius: 50%; 
+                        border:1px solid;
+                        display: block;
+                        margin: 0px auto;' alt='profile'>";
                     }
 
                     ?>
-
                 </div>
-                <br>
 
-                <div class="col-sm-6">
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title">Estado</h5>
-                            <?php
-
-                            foreach ($result2 as $r2) {
-                                $r2['estado'];
-                            }
-                            if (empty($r2)) {
-                                echo "<p class='card-text'>Hola, Actualiza tu estado.</p>";
-                            } else {
-                                echo "<p class='card-text'>" . $r2['estado'] . "</p>";
-                            }
-
-                            ?>
-
-                        </div>
-                    </div>
-                </div>
+                    <br>
             </div>
 
 
+            <div class="col-sm-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Estado</h5>
+                        <?php
+
+                        foreach ($result2 as $r2) {
+                            $r2['estado'];
+                        }
+                        if (empty($r2)) {
+                            echo "<p class='card-text'>Hola, Actualiza tu estado.</p>";
+                        } else {
+                            echo "<p class='card-text'>" . $r2['estado'] . "</p>";
+                        }
+
+                        ?>
+
+                    </div>
+                </div>
+            </div>
         </div>
+
+
+
 
         <?php require_once "Vistas/Layouts/Footer.php"; ?>
     </div>
